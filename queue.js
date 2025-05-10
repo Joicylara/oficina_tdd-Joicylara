8a14b202b0ba3eb214df2a63bd03d5058bae8e62
joicy kelly <joicykelly2301@gmail.com>
2025-05-10 17:52:20 -0300

    feat: adicionando as funcionalidades e a implemtação dos testes

--- queue.js
+++ queue.js
@@ -1,10 +1,25 @@
 class Queue {
-  elements
+  elements;
   constructor(){
-    this.elements = []
+    this.elements = [];
   }
   
-  // Implementar aqui
+  size(){
+    return this.elements.length;
+  }
+
+  add(item){
+    return this.elements.push(item);
+  }
+
+  peek(){
+    return this.elements[0];
+  }
+
+  dequeue(){
+    return this.elements.shift();
+  }
+ 
 }
 
 module.exports = Queue
--- queue.test.js
+++ queue.test.js
@@ -2,24 +2,52 @@ const Queue = require('./queue')
 
 describe('Queue', () => {
   it('Deve criar uma fila', () => {
-    const queue = new Queue()
+    const queue = new Queue();
 
-    expect(queue).toEqual({ elements: []})
+    expect(queue).toEqual({ elements: [] });
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+
+    queue.add('Morango');
+    queue.add('Banana');
+    queue.add('Goiaba');
+    queue.add('Manga');
+    queue.add('Maça');
+    queue.add('Mamão');
+
+    expect(queue.size()).toEqual(6);
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue();
+
+    expect(queue.add('Uva')).toEqual(1);
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+
+    queue.add('Morango');
+    queue.add('Banana');
+    queue.add('Goiaba');
+    queue.add('Manga');
+    queue.add('Maça');
+    queue.add('Mamão');
+
+    expect(queue.peek()).toEqual('Morango');
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.add('Morango');
+    queue.add('Banana');
+    queue.add('Goiaba');
+    queue.add('Manga');
+    queue.add('Maça');
+    queue.add('Mamão');
+
+    expect(queue.dequeue()).toEqual('Morango');
   })
 })
